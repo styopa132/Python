5e545da4351b41c27b52c76f36281634ecdadf8f
styopa132 <64314547+styopa132@users.noreply.github.com>
2022-09-22 19:18:36 +0300

    Python

--- 1 задание.py
+++ 1 задание.py
@@ -1,227 +1,25 @@
-import math
-from decimal import Decimal, ROUND_HALF_UP, ROUND_DOWN
-print('Введите действие')
-print('Сложение, вычитание, умножение, деление, возведение в степень, логарифм, округление в большую сторону до N знака после запятой, округление в меньшую сторону до N знака после запятой.')
-a = input()
-def plus():
-        print('Введите первое число')
-        q = input()
-        while True:
-            try:
-                float(q)
-                q = int(q)
-                break
-            except ValueError:
-                print('Введенный элемент не является числом, введите первый элемент')
-                q = input()
-        print('Введите второе число')
-        w = input()
-        while True:
-            try:
-                float(w)
-                w = int(w)
-                break
-            except ValueError:
-                print('Введенный элемент не является числом, введите второй элемент')
-                w = input()
-        print('Ответ =',q+w)
-def minus():
-    print('Введите первое число')
-    q = input()
-    while True:
-        try:
-            float(q)
-            q = int(q)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите первый элемент')
-            q = input()
-    print('Введите второе число')
-    w = input()
-    while True:
-        try:
-            float(w)
-            w = int(w)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите второй элемент')
-            w = input()
-    print('Ответ =', q - w)
-def umn():
-    print('Введите первое число')
-    q = input()
-    while True:
-        try:
-            float(q)
-            q = int(q)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите первый элемент')
-            q = input()
-    print('Введите второе число')
-    w = input()
-    while True:
-        try:
-            float(w)
-            w = int(w)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите второй элемент')
-            w = input()
-    print('Ответ =', q * w)
-def del1():
-    print('Введите первое число')
-    q = input()
-    while True:
-        try:
-            float(q)
-            q = int(q)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите первый элемент')
-            q = input()
-    print('Введите второе число')
-    w = input()
-    while True:
-        try:
-            float(w)
-            w = int(w)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите второй элемент')
-            w = input()
-    while True:
-        try:
-            w = int(w)
-            print(q/w)
-            break
-        except ZeroDivisionError:
-            print('Деление на ноль невозможно, введите второй элемент')
-            w = input()
-def step():
-    print('Введите первое число')
-    q = input()
-    while True:
-        try:
-            float(q)
-            q = int(q)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите первый элемент')
-            q = input()
-    print('Введите второе число')
-    w = input()
-    while True:
-        try:
-            float(w)
-            w = int(w)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите второй элемент')
-            w = input()
-    print('Ответ =', q ** w)
-def log():
-    while True:
-        while True:
-            print('Введите основание логарифма')
-            n = input()
-            try:
-                float(n)
-                n = int(n)
-                break
-            except ValueError:
-                print('Введенный элемент не является числом, введите основание логарифма')
-                n = input()
-        if n == 0:
-            print('Основание логарифма не может быть равно 0')
-        elif n == 1:
-            print('Основание логарифма не может быть равно 1')
-        elif n < 0:
-            print('Основание логарифма не может быть отрицательным числом')
-        else:
-            break
-    while True:
-        print('Введите аргумент логарифма')
-        m = input()
-        while True:
-            try:
-                float(m)
-                m = int(m)
-                break
-            except ValueError:
-                print('Введенный элемент не является числом, введите аргумент логарифма')
-                m = input()
-        if m == 0:
-            print('Аргумент логарифма не может быть равен 0')
-        elif m == 1:
-            print('Аргумент логарифма не может быть равен 1')
-        elif m < 0:
-            print('Аргумент логарифма не может быть отрицательным числом')
-        else:
-            break
-    print(math.log(m, n))
-def bol():
-    print('Введите число')
-    n = input()
-    while True:
-        try:
-            float(n)
-            n = float(n)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите число')
-            n = input()
-    print('До какого знака необходимо округлить?')
-    m = input()
-    while True:
-        try:
-            float(m)
-            m = int(m)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите число')
-            m = input()
-    b = '1.' + '0' * m
-    number = Decimal(n)
-    print(number.quantize(Decimal(b), ROUND_HALF_UP))
-def mal():
-    print('Введите число')
-    n = input()
-    while True:
-        try:
-            float(n)
-            n = float(n)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите число')
-            n = input()
-    print('До какого знака необходимо округлить?')
-    m = input()
-    while True:
-        try:
-            float(m)
-            m = int(m)
-            break
-        except ValueError:
-            print('Введенный элемент не является числом, введите число')
-            m = input()
-    b = '1.' + '0' * m
-    number = Decimal(n)
-    print(number.quantize(Decimal(b), ROUND_DOWN))
-if a == 'Сложение':
-    plus()
-if a == 'вычитание':
-    minus()
-if a == 'умножение':
-    umn()
-if a == 'деление':
-    del1()
-if a == 'возведение в степень':
-    step()
-if a == 'логарифм':
-    log()
-if a == 'округление в большую сторону до N знака после запятой':
-    bol()
-if a == 'округление в меньшую сторону до N знака после запятой':
-    mal()
+# Команда с числом
+print((1 + 2) * 3)
+# Команда со словом
+slovo = 'Welcome'
+for i in slovo:
+    print(i)
+# Команда со строкой
+stroka = 'Welcome to hell '
+print(stroka * 10)
+# Команда со списком
+a = []
+for i in range(15):
+    a.append(i)
+print(a)
+# Команда с кортежем
+def kort():
+    name = "Adam"
+    age = 44
+    band = "Three Days Grace"
+    return name, age, band
+user = kort()
+print(user)
+
+
 
--- 2 задание.py
+++ 2 задание.py
@@ -5,7 +5,15 @@ print('Сложение, вычитание, умножение, деление,
 a = input()
 def plus():
         print('Введите первое число')
-        q = int(input())
+        q = input()
+        while True:
+            try:
+                float(q)
+                q = int(q)
+                break
+            except ValueError:
+                print('Введенный элемент не является числом, введите первый элемент')
+                q = input()
         print('Введите второе число')
         w = input()
         while True:
@@ -19,7 +27,15 @@ def plus():
         print('Ответ =',q+w)
 def minus():
     print('Введите первое число')
-    q = int(input())
+    q = input()
+    while True:
+        try:
+            float(q)
+            q = int(q)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите первый элемент')
+            q = input()
     print('Введите второе число')
     w = input()
     while True:
@@ -33,7 +49,15 @@ def minus():
     print('Ответ =', q - w)
 def umn():
     print('Введите первое число')
-    q = int(input())
+    q = input()
+    while True:
+        try:
+            float(q)
+            q = int(q)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите первый элемент')
+            q = input()
     print('Введите второе число')
     w = input()
     while True:
@@ -47,7 +71,15 @@ def umn():
     print('Ответ =', q * w)
 def del1():
     print('Введите первое число')
-    q = int(input())
+    q = input()
+    while True:
+        try:
+            float(q)
+            q = int(q)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите первый элемент')
+            q = input()
     print('Введите второе число')
     w = input()
     while True:
@@ -58,10 +90,25 @@ def del1():
         except ValueError:
             print('Введенный элемент не является числом, введите второй элемент')
             w = input()
-    print('Ответ =', q / w)
+    while True:
+        try:
+            w = int(w)
+            print(q/w)
+            break
+        except ZeroDivisionError:
+            print('Деление на ноль невозможно, введите второй элемент')
+            w = input()
 def step():
     print('Введите первое число')
-    q = int(input())
+    q = input()
+    while True:
+        try:
+            float(q)
+            q = int(q)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите первый элемент')
+            q = input()
     print('Введите второе число')
     w = input()
     while True:
@@ -74,24 +121,90 @@ def step():
             w = input()
     print('Ответ =', q ** w)
 def log():
-    print("Введите основание логарифма")
-    n = int(input())
-    print("Введите аргумент логарифма")
-    m = int(input())
+    while True:
+        while True:
+            print('Введите основание логарифма')
+            n = input()
+            try:
+                float(n)
+                n = int(n)
+                break
+            except ValueError:
+                print('Введенный элемент не является числом, введите основание логарифма')
+                n = input()
+        if n == 0:
+            print('Основание логарифма не может быть равно 0')
+        elif n == 1:
+            print('Основание логарифма не может быть равно 1')
+        elif n < 0:
+            print('Основание логарифма не может быть отрицательным числом')
+        else:
+            break
+    while True:
+        print('Введите аргумент логарифма')
+        m = input()
+        while True:
+            try:
+                float(m)
+                m = int(m)
+                break
+            except ValueError:
+                print('Введенный элемент не является числом, введите аргумент логарифма')
+                m = input()
+        if m == 0:
+            print('Аргумент логарифма не может быть равен 0')
+        elif m == 1:
+            print('Аргумент логарифма не может быть равен 1')
+        elif m < 0:
+            print('Аргумент логарифма не может быть отрицательным числом')
+        else:
+            break
     print(math.log(m, n))
 def bol():
     print('Введите число')
-    n = float(input())
+    n = input()
+    while True:
+        try:
+            float(n)
+            n = float(n)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите число')
+            n = input()
     print('До какого знака необходимо округлить?')
-    m = int(input())
+    m = input()
+    while True:
+        try:
+            float(m)
+            m = int(m)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите число')
+            m = input()
     b = '1.' + '0' * m
     number = Decimal(n)
     print(number.quantize(Decimal(b), ROUND_HALF_UP))
 def mal():
     print('Введите число')
-    n = float(input())
+    n = input()
+    while True:
+        try:
+            float(n)
+            n = float(n)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите число')
+            n = input()
     print('До какого знака необходимо округлить?')
-    m = int(input())
+    m = input()
+    while True:
+        try:
+            float(m)
+            m = int(m)
+            break
+        except ValueError:
+            print('Введенный элемент не является числом, введите число')
+            m = input()
     b = '1.' + '0' * m
     number = Decimal(n)
     print(number.quantize(Decimal(b), ROUND_DOWN))
